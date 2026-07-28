2822a4a62a3c82ae2434ed7b141733b3ecaa52d4
Emanuil Ganchev <emanuil.d.ganchev@gmail.com>
2026-07-28 11:21:58 +0300

    lint: replace TextChanged with InsertLeave to avoid linting on every keystroke

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -9,7 +9,7 @@ return {
             lua = { "luacheck" },
         }
 
-        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "TextChanged" }, {
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
             callback = function()
                 require("lint").try_lint(nil, {
                     ignore_errors = true,
